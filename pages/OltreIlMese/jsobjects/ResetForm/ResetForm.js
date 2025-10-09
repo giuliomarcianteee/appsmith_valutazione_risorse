5624dadc213854e72955a2b63f5791fe7e94c276
attiliobut.gdv <attiliobut.gdv@gmail.com>
2025-10-09 15:47:37 +0000

    jsobject per reset form

---
+++ pages/OltreIlMese/jsobjects/ResetForm/ResetForm.js
@@ -0,0 +1,80 @@
+export default {
+  resetForm() {
+    resetWidget("RatingAccoglienza");
+    resetWidget("Radio1");
+    resetWidget("Radio2");
+    resetWidget("Radio3");
+    resetWidget("Radio4");
+    resetWidget("InputAccoglienza");
+
+    resetWidget("RatingBisogno");
+    resetWidget("Radio5");
+    resetWidget("Radio6");
+    resetWidget("Radio7");
+    resetWidget("Radio8");
+    resetWidget("InputBisogno");
+
+    resetWidget("RatingProdottiGdV");
+    resetWidget("Radio9");
+    resetWidget("Radio10");
+    resetWidget("Radio11");
+    resetWidget("Radio12");
+    resetWidget("InputProdottiGdV");
+
+    resetWidget("RatingProdotti");
+    resetWidget("Radio13");
+    resetWidget("Radio14");
+    resetWidget("Radio15");
+    resetWidget("Radio16");
+    resetWidget("InputProdotti");
+
+    resetWidget("RatingConsulenza");
+    resetWidget("Radio17");
+    resetWidget("Radio18");
+    resetWidget("Radio19");
+    resetWidget("Radio20");
+    resetWidget("InputConsulenza");
+
+    resetWidget("RatingPromozioni");
+    resetWidget("Radio21");
+    resetWidget("Radio22");
+    resetWidget("Radio23");
+    resetWidget("Radio24");
+    resetWidget("InputPromozioni");
+
+    resetWidget("RatingObiezioni");
+    resetWidget("Radio25");
+    resetWidget("Radio26");
+    resetWidget("Radio27");
+    resetWidget("Radio28");
+    resetWidget("InputObiezioni");
+
+    resetWidget("RatingChiusuraVendita");
+    resetWidget("Radio29");
+    resetWidget("Radio30");
+    resetWidget("Radio31");
+    resetWidget("Radio32");
+    resetWidget("InputChiusuraVendita");
+
+    resetWidget("RatingTeam");
+    resetWidget("Radio33");
+    resetWidget("Radio34");
+    resetWidget("Radio35");
+    resetWidget("Radio36");
+    resetWidget("InputTeam");
+
+    resetWidget("RatingOrganizzazione");
+    resetWidget("Radio37");
+    resetWidget("Radio38");
+    resetWidget("Radio39");
+    resetWidget("Radio40");
+    resetWidget("InputOrganizzazione");
+
+    resetWidget("RatingProcedure");
+    resetWidget("Radio41");
+    resetWidget("Radio42");
+    resetWidget("Radio43");
+    resetWidget("Radio44");
+    resetWidget("InputProcedure");
+  }
+}
