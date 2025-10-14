96b0f4edb9bd8f0051d77984b2fe748d8b513438
attiliobut.gdv <attiliobut.gdv@gmail.com>
2025-10-14 08:53:24 +0000

    testing

---
+++ pages/ValutazioneNegozio/jsobjects/ResetPassaggiNegozi/ResetPassaggiNegozi.js
@@ -0,0 +1,23 @@
+export default {
+  reset: () => {
+    InputPosizione.setValue("");
+    RatingPosizione.setValue(0);
+
+    InputAffluenza.setValue("");
+    RatingAffluenza.setValue(0);
+
+    InputClientela.setValue("");
+    RatingClientela.setValue(0);
+
+    InputVenditaMerceologica.setValue("");
+    InputAnalisiNegozio.setValue("");
+
+    InputPuliziaNegozio.setValue("");
+    RatingPuliziaNegozio.setValue(0);
+
+    InputEsposizione.setValue("");
+    RatingEsposizione.setValue(0);
+
+    InputAltro.setValue("");
+  }
+}
